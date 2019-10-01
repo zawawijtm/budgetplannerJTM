3187ffa50dfa994b2939ca7fdeab1e818eb689c4
zorro <awie.com.my@gmail.com>
2019-10-01 15:30:12 +0800

    update

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+
+  selector: 'app-home',
+
+  templateUrl: 'home.page.html',
+
+  styleUrls: ['home.page.scss'],
+
+})
+
+export class HomePage {
+
+  enteredReason = '';
+
+  inputAmount = '';
+
+  expensesList = [];
+
+
+
+  constructor() { }
+
+
+
+  onClick() {
+
+    alert('Button clicked');
+
+  }
+
+
+
+  resetForm() {
+
+    this.enteredReason = '';
+
+    this.inputAmount = '';
+
+  }
+
+
+
+  addExpense() {
+
+    this.expensesList.push(this.enteredReason + ' : RM' + this.inputAmount);
+
+  }
+
+
+
+  removeItem() { }
+
+
+
+}
